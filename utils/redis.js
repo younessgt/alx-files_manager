1221ce4bdc48539b57dbc0f77ba229a7ddfa053a
younessgt <youness.merzak123@gmail.com>
2024-03-07 15:08:39 +0100

    task0

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (err) => {
+      console.log(`Error from redis client: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
